4ff8e043522a7ad5f6d2da2820585d4294ac40ec
KiowaFG <kiowacdq@gmail.com>
2024-05-20 16:38:14 +0200

    starting

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,10 @@
 import "../App.css"
+import pasos from "../assets/images/b5adb323006f35f804937799be07efb8.png"
+
 function Navbar(){
     return (
         <nav className="navbar">
+            <img src={pasos} alt="" />
 
         </nav>
     )
